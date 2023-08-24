11bdd0142e11835c4eb7a295f3f548061f813ad2
bdonys <brandonsur@yahoo.com>
2023-08-24 14:42:16 -0700

    structured comments to start. added characters

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Write function to generate password
+// must be at least 8 characters long and no more than 128 characters
+function generatePassword() {
+
+  // Grab all variables for characters
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialChars = "!@#$%^&*(),./;'<>?:-_=+[]{}\|";
+
+  // prompt for the length of the password
+
+  // Prompt for lowercase, uppercase, numbers, and special characters
+
+  
+
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
